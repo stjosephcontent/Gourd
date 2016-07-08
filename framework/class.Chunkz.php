83bb8fce870b56756d55d76098735a9ea281c6fe
Sean Macdonald <sean@crazyhorsecoding.com>
2016-07-08 00:38:00 -0400

    clean up cruft

--- framework/class.Chunkz.php
+++ framework/class.Chunkz.php
@@ -110,22 +110,21 @@ class Chunkz extends Core {
 				if (isset($pstack['Chunks'][$Plane]['CUID'])) {
 					$PCUID = $pstack['Chunks'][$Plane]['CUID'];	
 				}
-				$crumbbread = array_merge($crumbbread,$this->generateAddressFromCUID($PCUID,$Plane));	
-                                echo "<pre>";print_r($crumbbread);  echo "</pre>";
+				$crumbbread = array_merge($crumbbread,$this->generateAddressFromCUID($PCUID,$Plane));
 				for ($i = 1; $i < sizeof($crumbbread);$i++) {
 					$h = $i +- 1;
-//                                        echo "<pre>";print_r($crumbbread[$i]['URN_en']);  echo "</pre>";
-                                        if ($crumbbread[$h]['URN_en'] == "BIM-Services") {
-                                            $crumbbread[$h]['URN_en'] = "services" . '#' . $crumbbread[$h]['URN_en'];
-                                        } else if ($crumbbread[$i]['URN_en'] == "Who-We-Are") {
-                                             $crumbbread[$h]['URN_en'] = "about" . '#' . $crumbbread[$h]['URN_en'];    
-                                        } else if ($crumbbread[$i]['URN_en'] == "Client-Services") {
-                                             $crumbbread[$h]['URN_en'] = "services" . '#' . $crumbbread[$h]['URN_en'];
-                                        } else {
-//                                            var_dump("here");
-                                            $crumbbread[$h]['URN_en'] = $crumbbread[$i]['URN_en'] . '/' . $crumbbread[$h]['URN_en'];
-                                        }
-					
+					/*
+					//	Weird Custom Mike Yoo Code
+					if ($crumbbread[$h]['URN_en'] == "BIM-Services") {
+						$crumbbread[$h]['URN_en'] = "services" . '#' . $crumbbread[$h]['URN_en'];
+					} else if ($crumbbread[$i]['URN_en'] == "Who-We-Are") {
+						$crumbbread[$h]['URN_en'] = "about" . '#' . $crumbbread[$h]['URN_en'];    
+					} else if ($crumbbread[$i]['URN_en'] == "Client-Services") {
+						$crumbbread[$h]['URN_en'] = "services" . '#' . $crumbbread[$h]['URN_en'];
+					} else {
+						$crumbbread[$h]['URN_en'] = $crumbbread[$i]['URN_en'] . '/' . $crumbbread[$h]['URN_en'];
+					}
+					*/
 				}
 			}
 		}
@@ -149,20 +148,13 @@ class Chunkz extends Core {
 			$c		= array("Chunks.$Plane.SEOTitle_$lang" => $addrbit,"Chunks.$Plane.PSUID" => (string) $PSUID);
 			$c2		= array("Chunks.0.SEOTitle_$lang" => $addrbit,"Chunks.0.PSUID" => (string) $PSUID);
 			$f		= array("Chunks");
-			
 			$this_plane = $Plane;
-			
 			$this_stack = $this->mongodb->stacks->findOne($c,$f);
-			
 			if ($Plane > 0 && empty($this_stack)) {
 				$this_stack = $this->mongodb->stacks->findOne($c2,$f);
 				$this_plane = 0;
 			} 
-			
-			//if ($this_stack = $this->mongodb->stacks->findOne($c,$f)) {
-			
 			if ($this_stack) {
-			
 				//	set parentage | set invalid URLs to zero-length strings
 				$this_URI_en[] = $this_stack['Chunks'][$this_plane]['SEOTitle_en'];
 				$this_URI_fr[] = $this_stack['Chunks'][$this_plane]['SEOTitle_fr'];
@@ -183,9 +175,7 @@ class Chunkz extends Core {
 					'URN_fr'		=> implode('/',$this_URI_fr),
 					'id'			=> $this_stack['Chunks'][$this_plane]['CUID'],
 					'ResourceType'	=> 'Chunk'
-					//'PSUID'			=> $this_stack['_id'],	//	prolly not required, but wouldn't hurt
 				);
-				
 				$PSUID = $this_stack['_id'];
 			} else {
 				$r = false;
@@ -193,21 +183,13 @@ class Chunkz extends Core {
 				$c		= array("Chunks.$Plane.SEOTitle_$lang" => $addrbit,"Chunks.$Plane.PSUID" => '');
 				$c2		= array("Chunks.0.SEOTitle_$lang" => $addrbit,"Chunks.0.PSUID" => '');	
 				$f		= array("Chunks");
-				
 				$this_plane = $Plane;
-			
 				$this_stack = $this->mongodb->stacks->findOne($c,$f);
-			
 				if ($Plane > 0 && !$this_stack) {
 					$this_stack = $this->mongodb->stacks->findOne($c2,$f);
 					$this_plane = 0;
 				} 
-			
-				//if ($this_stack = $this->mongodb->stacks->findOne($c,$f)) {
-			
 				if ($this_stack) {
-				
-				//if ($this_stack = $this->mongodb->stacks->findOne($c,$f)) {
 					$r = array(array(
 						'Title_en'		=> $this_stack['Chunks'][$this_plane]['Title_en'],
 						'Title_fr'		=> $this_stack['Chunks'][$this_plane]['Title_fr'],
@@ -222,8 +204,7 @@ class Chunkz extends Core {
 		return $r;
 	}
 	
-	public function flatFamilyTree($tree,$prefix='') {
-		$prefix_delim = ' ➛ ';
+	public function flatFamilyTree($tree,$prefix='',$prefix_delim=' ➛ ') {
 		$r = array();
 		foreach ($tree as $branch) {
 			$thisnode = $branch;
@@ -301,7 +282,6 @@ class Chunkz extends Core {
 		foreach ($stack['Chunks'] as $chunk) {
 			if (!empty($chunk['CUID']) && $chunk['CUID'] == $CUID) $r = $chunk;
 		}
-		//$r			= $stack['Chunks'][$Plane];
 		$r['ModuleID']	= $stack['ModuleID'];
 		$r['SUID']		= $stack['_id'];
 		return $r;
